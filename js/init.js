d531cacec3084d8d8155ef503aa5ba1d968f3269
SUJITH THOMAS <sujiththomas@SUJITHs-MacBook-Pro.local>
2021-04-28 09:32:35 +0530

    sections, ready for 1st revision

--- js/init.js
+++ js/init.js
@@ -4,6 +4,17 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+// $( document ).ready(function() {
+//   wow = new WOW(
+//     {
+//     boxClass:     'wow',      // default
+//     animateClass: 'animated', // default
+//     offset:       0,          // default
+//     mobile:       true,       // default
+//     live:         true        // default
+//   }
+//   )
+//   wow.init();
+// });
